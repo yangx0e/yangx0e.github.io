4901a9693c67210e579f7f4e3fcde987b9b7d626
yangx0e <57872966+yangx0e@users.noreply.github.com>
2025-11-29 21:35:14 +0000

    Deploying to gh-pages from @ yangx0e/yangx0e.github.io@9e0f879d67ec80bfe2f1c6e2086fb4d7a603ec55 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
